3eccd3b3557a350b233ebd85f26b934c882432f8
victormarius <100percentfragrance@gmail.com>
2022-03-19 12:52:45 +0100

    second day

---
+++ config.py
@@ -0,0 +1,1 @@
+GCP_CONNECT_KEY = 'keys.json'
---
+++ connectToGcloud.py
@@ -0,0 +1,19 @@
+import os
+import json
+from google.cloud import datastore
+from google.oauth2 import service_account
+
+
+# GCP_CONNECT_KEY = os.environ['GCP_CONNECT_KEY']
+
+print(os.getenv('GCP_CONNECT_KEY'))
+
+# KEYS = json.loads(GCP_CONNECT_KEY)  # convert JSON to dictionary
+#
+# credentials = service_account.Credentials.from_service_account_file(KEYS)
+#
+# datastore_client = datastore.Client(credentials=credentials)
+#
+# query = datastore_client.query(kind='EndUsers')
+# for entity in query.fetch(limit=5):
+#     print(entity)
