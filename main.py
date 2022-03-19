decee9902c407f28dd165b9ecd77a596e977ad3b
victormarius <100percentfragrance@gmail.com>
2022-03-19 13:42:17 +0100

    second second trial

--- main.py
+++ main.py
@@ -7,10 +7,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    name = os.environ.get("NAME", "World")
     first5 = gcp.getData()
     print(first5)
-    return "Hello {}! Just Making sure it is working twice".format(first5)
+    return "Hello  Just Making sure it is working twice {}".format(first5)
 
 
 if __name__ == "__main__":
