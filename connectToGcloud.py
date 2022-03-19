af9c2af318e798b1efebfc9cf6e56f8d21b3a396
victormarius <100percentfragrance@gmail.com>
2022-03-19 19:44:49 +0100

    initial project

--- connectToGcloud.py
+++ connectToGcloud.py
@@ -2,21 +2,15 @@ import os
 import json
 from google.cloud import datastore
 from google.oauth2 import service_account
-from dotenv import load_dotenv
 
 
 def getData():
-    load_dotenv()
-    GCP_CONNECT_KEY = os.environ['GCP_CONNECT_KEY']
-    # KEYS = json.loads(GCP_CONNECT_KEY)  # convert JSON to dictionary
     credentials = service_account.Credentials.from_service_account_file('keys.json')
     datastore_client = datastore.Client(credentials=credentials)
     retValue = ''
     query = datastore_client.query(kind='EndUsers')
     for entity in query.fetch(limit=5):
         retValue += str(entity)
-        # print(entity)
     return retValue
-
 
 # print(getData())
